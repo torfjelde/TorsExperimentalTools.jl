962d286872f33fa84e8cb6dfce0dac1131c11579
Documenter.jl <documenter@juliadocs.github.io>
2023-09-09 10:14:06 +0000

    build based on 13ad696

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TorsExperimentalTools","category":"page"},{"location":"#TorsExperimentalTools","page":"Home","title":"TorsExperimentalTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TorsExperimentalTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TorsExperimentalTools]","category":"page"}]
+}
